3cba484851ac9c7dad0bd2b9c6d7d99f2cff73f2
Laxmiprasad Chilukuru <126027863+laxmiprasadchilukuru@users.noreply.github.com>
2023-04-04 15:11:18 -0500

    commit

--- main.tf
+++ main.tf
@@ -35,7 +35,6 @@ resource "aws_security_group" "blog" {
 
   vpc_id = data.aws_vpc.default.id
 }
-
 resource "aws_security_group_rule" "blog_http_in" {
   type        = "ingress"
   from_port   = 80
@@ -55,6 +54,7 @@ resource "aws_security_group_rule" "blog_https_in" {
 
   security_group_id = aws_security_group.blog.id
 }
+
 resource "aws_security_group_rule" "blog_everything_out" {
   type        = "egrees"
   from_port   = 0
